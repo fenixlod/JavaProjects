35febee98b2c78f81117a0bbb404f9192a9b62a1
fenixlod <fenix_lod@abv.bg>
2017-11-28 21:09:01 +0200

    Added DB

--- Books/src/main/java/com/project/books/additional/Configuration.java
+++ Books/src/main/java/com/project/books/additional/Configuration.java
@@ -1,9 +1,13 @@
 package com.project.books.additional;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import com.project.books.data.BooksDataStorageDB;
 import com.project.books.data.BooksDataStorageInMemory;
 import com.project.books.data.IBooksDataStorage;
 
@@ -11,9 +15,29 @@ import com.project.books.data.IBooksDataStorage;
 @ComponentScan(basePackages="com.project.books")
 @EnableWebMvc
 public class Configuration extends WebMvcConfigurerAdapter {
+	private DataStrorageType DSType = DataStrorageType.DATABASE;
 	
 	@Bean
-    public IBooksDataStorage getDataSorce() {
-        return new BooksDataStorageInMemory();
+    public IBooksDataStorage getDataSourceInstance() {
+		if(DSType == DataStrorageType.INMEMORY) {
+			return new BooksDataStorageInMemory();
+		}
+		else {
+			return new BooksDataStorageDB(getDataSource());
+		}
     }
+	
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/BooksDB");
+        dataSource.setUsername("root");
+        //dataSource.setPassword("P@ssw0rd");
+        return dataSource;
+    }
+}
+
+enum DataStrorageType {
+	INMEMORY,DATABASE
 }
---
+++ Books/src/main/java/com/project/books/data/BooksDataStorageDB.java
@@ -0,0 +1,119 @@
+package com.project.books.data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.project.books.models.Book;
+
+public class BooksDataStorageDB implements IBooksDataStorage {
+
+	private JdbcTemplate dbTemplate;
+	
+	@Autowired
+	public BooksDataStorageDB(DataSource source) {
+		dbTemplate = new JdbcTemplate(source);
+	}
+	
+	@Override
+	public List<Book> getAllBooks() {
+		try {
+			String sql = "SELECT * FROM books";
+			List<Book> allBooks = dbTemplate.query(sql, new RowMapper<Book>() {
+		 
+		        @Override
+		        public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+		        	Book aBook = new Book();
+		        	aBook.setId(rs.getInt("id"));
+		        	aBook.setName(rs.getString("name"));
+		        	aBook.setAutor(rs.getString("author"));
+		            return aBook;
+		        }
+		    });
+			return allBooks;
+		}
+		catch(DataAccessException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public Book getBookById(int id) {
+		try {
+			String sqlQuery = "SELECT * FROM books WHERE id=" + id;
+			Book foundBook = dbTemplate.query(sqlQuery, new ResultSetExtractor<Book>() {
+
+				@Override
+				public Book extractData(ResultSet rs) throws SQLException, DataAccessException {
+					if(rs.next()) {
+						Book fBook = new Book();
+						fBook.setId(rs.getInt("id"));
+						fBook.setName(rs.getString("name"));
+						fBook.setAutor(rs.getString("author"));
+						return fBook;
+					}
+					else
+						return null;
+				}
+			});
+			return foundBook;
+		}
+		catch(DataAccessException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public List<Book> getBooksByName(String name) {
+		try {
+			String sqlQuery = "SELECT * FROM books WHERE name=" + name;
+			System.out.println(sqlQuery);
+			List<Book> foundBooks = dbTemplate.query(sqlQuery, new RowMapper<Book>() {
+
+				@Override
+				public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+					System.out.println("BOOK FOUND!");
+					Book aBook = new Book();
+					aBook.setId(rs.getInt("id"));
+					aBook.setName(rs.getString("name"));
+					aBook.setAutor(rs.getString("author"));
+					return aBook;
+				}
+			});
+			
+			if(foundBooks != null)
+				return foundBooks;
+			else
+				return new ArrayList<Book>();
+		}
+		catch(DataAccessException e) {
+			return new ArrayList<Book>();
+		}
+	}
+
+	@Override
+	public void addBook(Book newBook) {
+		String sql = "INSERT INTO books (id,name,author) VALUES(?,?,?)";
+		dbTemplate.update(sql,newBook.getId(),newBook.getName(),newBook.getAutor());
+	}
+
+	@Override
+	public void updateBook(Book updateBook) {
+		String sql = "UPDATE books SET name=?, author=? WHERE id=?";
+		dbTemplate.update(sql,updateBook.getName(),updateBook.getAutor(),updateBook.getId());
+	}
+
+	@Override
+	public boolean deleteBook(int id) {
+		String sql = "DELETE FROM books WHERE id=?";
+		dbTemplate.update(sql,id);
+		return true;
+	}
+}
